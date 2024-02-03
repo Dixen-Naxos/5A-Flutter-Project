1c87a62ebc61696b0ac970a6262546e3fd7b24b0
Dixen-Naxos <armanddailly@gmail.com>
2024-02-03 22:29:12 +0100

    Add image model

---
+++ lib/models/image.dart
@@ -0,0 +1,19 @@
+class Image {
+  final String path;
+  final String name;
+  final String type;
+  final int size;
+  final String mime;
+  final Object meta;
+  final String url;
+
+  Image({
+    required this.path,
+    required this.name,
+    required this.type,
+    required this.size,
+    required this.mime,
+    required this.meta,
+    required this.url,
+  });
+}
